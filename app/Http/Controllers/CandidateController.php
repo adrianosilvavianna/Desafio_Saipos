b6a7b53772b1d2f37c32f2674d5d8fa2ef0e3cbf
adrianosilvavianna <adriano276916@gmail.com>
2021-02-26 17:17:29 -0300

    fix install laravel - create Login - setup Vuejs

---
+++ app/Http/Controllers/CandidateController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Candidate;
+use Illuminate\Http\Request;
+use Facade\FlareClient\Http\Response;
+
+class CandidateController extends Controller
+{
+    private $candidate;
+
+    public function __construct(Candidate $candidate)
+    {
+        $this->candidate = $candidate;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response($this->candidate->all()->jsonSerialize(), 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Candidate  $candidate
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Candidate $candidate)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Candidate  $candidate
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Candidate $candidate)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Candidate  $candidate
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Candidate $candidate)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Candidate  $candidate
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Candidate $candidate)
+    {
+        //
+    }
+}
